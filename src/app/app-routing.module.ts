caede64045a4b9b13d4d2109a01739e8504daeee
Warit Santaputra <waritsan@gmail.com>
2019-08-09 15:29:35 +0700

    Add Calculator page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
-  }
+  },
+  { path: 'calculator', loadChildren: './calculator/calculator.module#CalculatorPageModule' }
 ];
 
 @NgModule({
