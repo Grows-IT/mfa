2a5cca0bef589b8e5253316c2879e355e5941d93
Warit Santaputra <waritsan@gmail.com>
2019-08-11 11:25:03 +0700

    Merge assess-sugar

--- src/app/assess-sugar/assess-sugar.page.ts
+++ src/app/assess-sugar/assess-sugar.page.ts
@@ -11,5 +11,22 @@ export class AssessSugarPage implements OnInit {
 
   ngOnInit() {
   }
-
+  public area;
+  public areaWidth;
+  public gap;
+  public caneBlock;
+  public totalWeightAll;
+  public totalOutput;
+  public showResult = false;
+  calculateTotalWeight() {
+    const areaLenght =this.area*1600/this.areaWidth
+    const totalGap = this.areaWidth/this.gap
+    const totalCaneBlock=areaLenght*totalGap*(12*this.caneBlock)
+    
+    const totalWeight=totalCaneBlock*0.002
+    //this.totalOutput=totalWeight/this.area
+    this.totalWeightAll=Math.floor(this.area*1600/this.areaWidth*this.areaWidth/this.gap*this.caneBlock*0.002)
+    this.totalOutput=Math.floor(this.area*1600/this.areaWidth*this.areaWidth/this.gap*this.caneBlock*0.002/this.area)
+    this.showResult = true;
+  }
 }
