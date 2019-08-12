99d7ddff794da13c3116002bf41395b543ff4600
Warit Santaputra <waritsan@gmail.com>
2019-08-12 10:23:29 +0700

    Add static page

---
+++ src/app/pages.ts
@@ -0,0 +1,9 @@
+export const pages = [{
+    id: '0',
+    name: 'รายละเอียดการคำนวณต่อ 1 ไร่',
+    img: 'assets/img/staticPages/farm-modernfarm-report.png'
+}, {
+    id: '1',
+    name: 'รายละเอียดการคำนวณต่อ 1 ไร่',
+    img: 'assets/img/staticPages/human-machine-report.png'
+}];
