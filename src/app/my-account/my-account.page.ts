3aa1f16531cca4893bed8d39dc57e634f90fd283
Warit Santaputra <waritsan@gmail.com>
2019-08-13 13:45:56 +0700

    Error cordova photo library

---
+++ src/app/my-account/my-account.page.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoLibrary } from '@ionic-native/photo-library/ngx';
+
+@Component({
+  selector: 'app-my-account',
+  templateUrl: './my-account.page.html',
+  styleUrls: ['./my-account.page.scss'],
+})
+export class MyAccountPage implements OnInit {
+
+  constructor(private photoLibrary: PhotoLibrary) {
+    this.photoLibrary.requestAuthorization().then(() => {
+      this.photoLibrary.getLibrary().subscribe({
+        next: library => {
+          library.forEach(function(libraryItem) {
+            console.log(libraryItem.id);          // ID of the photo
+            console.log(libraryItem.photoURL);    // Cross-platform access to photo
+            console.log(libraryItem.thumbnailURL);// Cross-platform access to thumbnail
+            console.log(libraryItem.fileName);
+            console.log(libraryItem.width);
+            console.log(libraryItem.height);
+            console.log(libraryItem.creationDate);
+            console.log(libraryItem.latitude);
+            console.log(libraryItem.longitude);
+            console.log(libraryItem.albumIds);    // array of ids of appropriate AlbumItem, only of includeAlbumsData was used
+          });
+        },
+        error: err => { console.log('could not get photos'); },
+        complete: () => { console.log('done getting photos'); }
+      });
+    })
+    .catch(err => console.log('permissions weren\'t granted'));
+  }
+
+  ngOnInit() {
+  }
+
+}
