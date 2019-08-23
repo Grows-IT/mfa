733f7175818f1b75c256ef70e63df01777073a3a
Warit Santaputra <waritsan@gmail.com>
2019-08-23 10:37:54 +0700

    Update calculators folder

--- src/app/calculators/calculators.page.ts
+++ src/app/calculators/calculators.page.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-calculator',
-  templateUrl: './calculator.page.html',
-  styleUrls: ['./calculator.page.scss'],
+  selector: 'app-calculators',
+  templateUrl: './calculators.page.html',
+  styleUrls: ['./calculators.page.scss'],
 })
-export class CalculatorPage implements OnInit {
+export class CalculatorsPage implements OnInit {
   calculators = [{
     id: '1',
     img: 'assets/img/icon-calculator-farm-modernfarm.png',
-    url: '/calculator/farm-vs-modern-farm'
+    url: '/calculators/farm-vs-modern-farm'
   }, {
     id: '2',
     img: 'assets/img/icon-calculator-human-machine.png',
-    url: '/calculator/human-vs-machine'
+    url: '/calculators/human-vs-machine'
   }, {
     id: '3',
     img: 'assets/img/icon-calculator-sugarcane.png',
-    url: '/calculator/assess-sugar'
+    url: '/calculators/assess-sugar'
   }];
 
   constructor() { }
