545fa19d6cb439f3a48b2cac7afdc05c0f7ce342
Warit Santaputra <waritsan@gmail.com>
2019-08-23 18:49:14 +0700

    Fix courses router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,27 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  { path: 'courses', loadChildren: './courses/courses.module#CoursesPageModule' },
-  { path: 'courses/:courseId/topics', loadChildren: './courses/topics/topics.module#TopicsPageModule' },
-  { path: 'courses/:courseId/topics/:topicId/activities',
-    loadChildren: './courses/topics/activities/activities.module#ActivitiesPageModule' },
-  { path: 'courses/:courseId/topics/:topicId/activities/:activityId/pages',
-    loadChildren: './courses/topics/activities/pages/pages.module#PagesPageModule' },
-  { path: 'calculators', loadChildren: './calculators/calculators.module#CalculatorsPageModule' },
-  { path: 'calculators/farm-vs-modern-farm',
-    loadChildren: './calculators/farm-vs-modern-farm/farm-vs-modern-farm.module#FarmVsModernFarmPageModule' },
-  { path: 'calculators/human-vs-machine', loadChildren: './calculators/human-vs-machine/human-vs-machine.module#HumanVsMachinePageModule' },
-  { path: 'calculators/assess-sugar', loadChildren: './calculators/assess-sugar/assess-sugar.module#AssessSugarPageModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
-  { path: 'static/:pageId', loadChildren: './static/static.module#StaticPageModule' },
-  { path: 'my-account', loadChildren: './my-account/my-account.module#MyAccountPageModule' }
+const routes: Routes = [{
+  path: '',
+  redirectTo: 'home',
+  pathMatch: 'full'
+}, {
+  path: 'home',
+  loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+}, {
+  path: 'courses',
+  loadChildren: './courses/courses.module#CoursesPageModule'
+}, {
+  path: 'calculators',
+  loadChildren: './calculators/calculators.module#CalculatorsPageModule'
+},
+  // { path: 'calculators/farm-vs-modern-farm',
+  //   loadChildren: './calculators/farm-vs-modern-farm/farm-vs-modern-farm.module#FarmVsModernFarmPageModule' },
+  // { path: 'calculators/human-vs-machine', loadChildren: './calculators/human-vs-machine/human-vs-machine.module#HumanVsMachinePageModule' },
+  // { path: 'calculators/assess-sugar', loadChildren: './calculators/assess-sugar/assess-sugar.module#AssessSugarPageModule' },
+  // { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  // { path: 'static/:pageId', loadChildren: './static/static.module#StaticPageModule' },
+  // { path: 'my-account', loadChildren: './my-account/my-account.module#MyAccountPageModule' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { IonicStorageModule } from '@ionic/storage';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -18,11 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    FormsModule,
-    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
-    IonicStorageModule.forRoot()
   ],
   providers: [
     StatusBar,
