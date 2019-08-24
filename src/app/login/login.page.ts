b7466b6fe39dd62b5508e1fe181f803f323e7f0f
Warit Santaputra <waritsan@gmail.com>
2019-08-24 12:07:56 +0700

    Combine auth files

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
-import { AuthService } from '../auth.service';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-login',
