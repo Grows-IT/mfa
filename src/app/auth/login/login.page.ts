239d92b0314ca30430fa8f0a76275509f5c9e7e7
Warit Santaputra <waritsan@gmail.com>
2019-08-24 16:08:57 +0700

    Add loading

--- src/app/auth/login/login.page.ts
+++ src/app/auth/login/login.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
 
+import { LoadingController } from '@ionic/angular';
+
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -11,14 +13,14 @@ import { AuthService } from '../auth.service';
 })
 export class LoginPage implements OnInit {
   errorMessage: string;
-  loginAttempt = 0;
+  isLoading = false;
   loginForm: FormGroup;
-  // users = [];
 
   constructor(
     private router: Router,
     private authService: AuthService,
     private formBuilder: FormBuilder,
+    private loadingCtrl: LoadingController
   ) {
     this.loginForm = this.formBuilder.group({
       username: ['', Validators.required],
@@ -26,30 +28,22 @@ export class LoginPage implements OnInit {
     });
   }
 
-  ngOnInit() {
-    // for (let index = 0; index < 30; index++) {
-    //   const user = {
-    //     id: index.toString(),
-    //     username: 'user' + index,
-    //     password: 'password' + index
-    //   }
-    //   this.users.push(user);
-    // }
-  }
+  ngOnInit() {}
 
   onSubmit(loginData: any): void {
-    this.authService.login(loginData.username, loginData.password, (err) => {
-      if (err) {
-        this.loginAttempt += 1;
-        return this.errorMessage = err + '. Login attempt: ' + this.loginAttempt;
-      }
-      // this.authService.getUserInfo((err, user) => {
-      //   if (err) return console.log(err);
-      //   console.log('Seccuess');
-      // });
-      // this.authService.getUserProfile().subscribe(user => console.log(user.firstname));
-      this.loginAttempt = 0;
-      this.router.navigate(['/home']);
+    this.isLoading = true;
+    this.loadingCtrl.create({
+      keyboardClose: true,
+      message: 'Logging in...'
+    }).then(loadingEl => {
+      loadingEl.present();
+      this.authService.login(loginData.username, loginData.password).subscribe(error => {
+        loadingEl.dismiss();
+        if (error) {
+          return this.errorMessage = error;
+        }
+        this.router.navigate(['/home']);
+      });
     });
   }
 }
