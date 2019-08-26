8061ee841abd871911d3ce4d81f32e515f4dedcd
Warit Santaputra <waritsan@gmail.com>
2019-08-26 13:08:52 +0700

    Save user data locally

--- src/app/auth/profile/profile.page.ts
+++ src/app/auth/profile/profile.page.ts
@@ -1,26 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { User } from '../user.model';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage implements OnInit, OnDestroy {
+  private userSub: Subscription;
+  private getSiteInfoSub: Subscription;
+  user: User;
 
   constructor(
-    private authService: AuthService,
-    private router: Router
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
-    this.authService.userProfile.subscribe(user => {
-      console.log(user);
-    }, error => {
-      console.log(error.message);
-      this.router.navigateByUrl('/auth/login');
+    this.userSub = this.authService.userProfile.subscribe(user => {
+      if (!user) {
+        this.getSiteInfoSub = this.authService.getSiteInfo().subscribe(u => {
+          this.user = u;
+        }, err => {
+          console.log('Error getSiteInfo(): ', err.message);
+        });
+      }
     });
+  }
+
+  ngOnDestroy() {
+    if (this.userSub) {
+      this.userSub.unsubscribe();
+    }
+    if (this.getSiteInfoSub) {
+      this.getSiteInfoSub.unsubscribe();
+    }
   }
 }
