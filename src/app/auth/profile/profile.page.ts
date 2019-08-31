51db9b100b6e96ce33f38d03b514a6126293d11b
Warit Santaputra <waritsan@gmail.com>
2019-08-31 20:08:29 +0700

    Add camera

--- src/app/auth/profile/profile.page.ts
+++ src/app/auth/profile/profile.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Plugins, Capacitor, CameraSource, CameraResultType } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
+import { AuthService } from '../auth.service';
 import { User } from '../user.model';
 
 @Component({
@@ -10,14 +12,20 @@ import { User } from '../user.model';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit, OnDestroy {
+  @ViewChild('filePicker', { static: false }) filePickerRef: ElementRef<HTMLInputElement>;
   private userSub: Subscription;
   user: User;
+  usePicker = false;
 
   constructor(
+    private platform: Platform,
     private authService: AuthService
   ) { }
 
   ngOnInit() {
+    if ((this.platform.is('mobile') && !this.platform.is('hybrid')) || this.platform.is('desktop')) {
+      this.usePicker = true;
+    }
     this.userSub = this.authService.user.subscribe(user => {
       this.user = user;
     });
@@ -27,5 +35,30 @@ export class ProfilePage implements OnInit, OnDestroy {
     if (this.userSub) {
       this.userSub.unsubscribe();
     }
+  }
+
+  onPickImage() {
+    if (!Capacitor.isPluginAvailable('Camera') || this.usePicker) {
+      this.filePickerRef.nativeElement.click();
+      return;
+    }
+    Plugins.Camera.getPhoto({
+      quality: 50,
+      source: CameraSource.Prompt,
+      correctOrientation: true,
+      width: 300,
+      resultType: CameraResultType.Uri
+    }).then(image => {
+      this.user.imgUrl = image.webPath;
+    }).catch(err => {
+      console.log(err);
+      if (this.usePicker) {
+        this.filePickerRef.nativeElement.click();
+      }
+    });
+  }
+
+  onFileChosen(event: Event) {
+    console.log(event);
   }
 }
