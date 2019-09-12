a00f0b89fed85e902094309d7af2453b1270fc0a
Warit Santaputra <waritsan@gmail.com>
2019-09-12 21:22:23 +0700

    Fix tabs routing

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
-import {RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { TabsPage } from './tabs.page';
 
 const routes: Routes = [{
   path: '',
-  redirectTo: 'home',
+  redirectTo: '/tabs/home',
   pathMatch: 'full'
 }, {
-  path: 'home',
-  loadChildren: '../home/home.module#HomePageModule'
+  path: '',
+  component: TabsPage,
+  children: [{
+    path: 'home',
+    loadChildren: '../home/home.module#HomePageModule'
+  }]
 }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class TabsRoutingModule {}
+export class TabsRoutingModule { }
