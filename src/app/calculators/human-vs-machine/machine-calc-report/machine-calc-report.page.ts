7d6b61b69a7c9abaca1f1e94d3cc3518f2254b06
nuiz37 <nuiz37@yahoo.com>
2019-09-18 16:38:12 +0700

    update report for calculators

---
+++ src/app/calculators/human-vs-machine/machine-calc-report/machine-calc-report.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-machine-calc-report',
+  templateUrl: './machine-calc-report.page.html',
+  styleUrls: ['./machine-calc-report.page.scss'],
+})
+export class MachineCalcReportPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
