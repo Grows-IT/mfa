5f0d052819c891c0ba6ca807a83fd8f5b005e645
Warit Santaputra <waritsan@gmail.com>
2019-09-19 14:00:07 +0700

    Remove unused var

--- src/app/news/news.service.ts
+++ src/app/news/news.service.ts
@@ -28,6 +28,7 @@ export class NewsService {
           return of(pages);
         }
         let pageNum: number;
+        let i = 0;
         return this.coursesService.courses.pipe(
           switchMap(courses => {
             const newsCourse = courses.find(course => course.identifier === 'news');
