9adfb119d48d8b6d8338f03e57d83c1fe536b0f8
Warit Santaputra <waritsan@gmail.com>
2019-09-21 13:54:01 +0700

    Add machine calculator report

--- src/app/calculators/human-vs-machine/human-vs-machine.page.ts
+++ src/app/calculators/human-vs-machine/human-vs-machine.page.ts
@@ -1,105 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { CalculatorsService, MachineCalcData } from '../calculators.service';
 
 @Component({
   selector: 'app-human-vs-machine',
   templateUrl: './human-vs-machine.page.html',
   styleUrls: ['./human-vs-machine.page.scss'],
-  
 })
 export class HumanVsMachinePage implements OnInit {
-  inputForm: FormGroup;
-  quantity: number;
-  totalPriceManPowerBurned: number;
-  totalPriceManPowerFresh: number;
-  totalPriceCartPower: number;
-  priceBurnedCart: number;
-  totalDayCartPower: number;
-  totalDayManPowerBurned: number;
-  totalDayManPowerFresh: number;
   showResult = false;
-
-  calsugarcaneCutPriceManPowerBurned: number;
-  calsugarcaneCutPriceManPowerFresh: number;
-  calsugarcaneCutPriceCartPower: number;
-  calsugarcaneGetPriceManPowerBurned: number;
-  calsugarcaneGetPriceManPowerFresh: number;
-  calsugarcaneGetPriceCartPower: number;
-  calsugarcaneLoadPriceManPowerBurned: number;
-  calsugarcaneLoadPriceManPowerFresh: number;
-  calsugarcaneLoadPriceCartPower: number;
-  calclearBurned: number;
+  inputForm: FormGroup;
+  result: MachineCalcData;
 
   constructor(
     private formBuilder: FormBuilder,
+    private calculatorsService: CalculatorsService
   ) {
     this.inputForm = this.formBuilder.group({
       quantity: ['', Validators.min(1)]
     });
-    
-   }
+  }
 
   ngOnInit() {
   }
-  hide1:boolean = false;
-  hide2:boolean = false;
-  hide3:boolean = false;
-  showDiv(divHide){
-    switch (divHide){
-      case "hide1": this.hide1=!this.hide1; break;
-      case "hide2": this.hide2=!this.hide2; break;
-      case "hide3": this.hide3=!this.hide3; break;
+
+  calculateTotalPrice(data: any) {
+    if (data.quantity <= 0) {
+      return;
     }
-  }
-  calculateTotalPrice(data) {
-    if (data.quantity <= 0) return;
-    this.quantity = data.quantity;
-    const sugarcaneCutPriceManPowerBurned = 100
-    const sugarcaneCutPriceManPowerFresh = 100
-    const sugarcaneCutPriceCartPower = 190
-
-    const sugarcaneGetPriceManPowerBurned = 100
-    const sugarcaneGetPriceManPowerFresh = 100
-    const sugarcaneGetPriceCartPower = 0
-
-    const sugarcaneLoadPriceManPowerBurned = 150
-    const sugarcaneLoadPriceManPowerFresh = 150
-    const sugarcaneLoadPriceCartPower = 150
-
-    const clearBurned = 30
-
-    const totalPerTonManPowerBurned = sugarcaneCutPriceManPowerBurned + sugarcaneGetPriceManPowerBurned + sugarcaneLoadPriceManPowerBurned + clearBurned
-    const totalPerTonManPowerFresh = sugarcaneCutPriceManPowerFresh + sugarcaneGetPriceManPowerFresh + sugarcaneLoadPriceManPowerFresh
-    const totalPerTonCartPower = sugarcaneCutPriceCartPower + sugarcaneGetPriceCartPower + sugarcaneLoadPriceCartPower
-
-    const cartPower = 400
-    const manPowerBurned = 12
-    const manPowerFresh = 4
-    const numberManPower = 10
-
-    this.totalDayCartPower = Math.ceil(this.quantity / cartPower)
-    this.totalDayManPowerBurned = Math.ceil(this.quantity / numberManPower / manPowerBurned)
-    this.totalDayManPowerFresh = Math.ceil(this.quantity / numberManPower / manPowerFresh)
-
-    this.totalPriceCartPower = Math.floor(this.quantity * totalPerTonCartPower)
-    this.totalPriceManPowerBurned = Math.floor(this.quantity * totalPerTonManPowerBurned)
-    this.totalPriceManPowerFresh = Math.floor(this.quantity * totalPerTonManPowerFresh)
-
-    this.priceBurnedCart = Math.floor(this.totalPriceManPowerBurned - this.totalPriceCartPower)
-    //this.priceFreshCart = Math.floor(totalPriceManPowerFresh - this.totalPriceCartPower)
+    this.result = this.calculatorsService.calculateMachine(data.quantity);
     this.showResult = true;
-
-    this.calsugarcaneCutPriceManPowerBurned=this.quantity*sugarcaneCutPriceManPowerBurned;
-    this.calsugarcaneCutPriceManPowerFresh=this.quantity*sugarcaneCutPriceManPowerFresh;
-    this.calsugarcaneCutPriceCartPower=this.quantity*sugarcaneCutPriceCartPower;
-    this.calsugarcaneGetPriceManPowerBurned=this.quantity*sugarcaneGetPriceManPowerBurned;;
-    this.calsugarcaneGetPriceManPowerFresh=this.quantity*sugarcaneGetPriceManPowerFresh;
-    this.calsugarcaneGetPriceCartPower=this.quantity*sugarcaneGetPriceCartPower;
-    this.calsugarcaneLoadPriceManPowerBurned=this.quantity*sugarcaneLoadPriceManPowerBurned;
-    this.calsugarcaneLoadPriceManPowerFresh=this.quantity*sugarcaneLoadPriceManPowerFresh;
-    this.calsugarcaneLoadPriceCartPower=this.quantity*sugarcaneLoadPriceCartPower;
-    this.calclearBurned=this.quantity*clearBurned;
-  
-
   }
 }
