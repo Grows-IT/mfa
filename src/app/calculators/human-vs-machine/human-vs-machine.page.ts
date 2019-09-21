be6e5917d9c0215d877ee9d18b482c97a4b12627
Warit Santaputra <waritsan@gmail.com>
2019-09-21 18:42:01 +0700

    Simplify farm modernfarm calculator

--- src/app/calculators/human-vs-machine/human-vs-machine.page.ts
+++ src/app/calculators/human-vs-machine/human-vs-machine.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
-import { CalculatorsService, MachineCalcData, HumanMachineData } from '../calculators.service';
+import { CalculatorsService, HumanMachineData } from '../calculators.service';
 
 @Component({
   selector: 'app-human-vs-machine',
