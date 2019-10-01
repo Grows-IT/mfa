58db6390c97f49b8d3a67827376c68535abd6bcc
Warit Santaputra <waritsan@gmail.com>
2019-10-01 09:55:02 +0700

    Seperate resource download and fetchTopic

--- src/app/knowledge-room/courses/topics/topics.page.ts
+++ src/app/knowledge-room/courses/topics/topics.page.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { CoursesService } from '../courses.service';
 import { Topic, Course } from '../course.model';
 import { Subscription } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topics',
@@ -18,10 +18,12 @@ export class TopicsPage implements OnInit, OnDestroy {
   errorMessage: string;
   private coursesSub: Subscription;
   private fetchSub: Subscription;
+  private downloadSub: Subscription;
+  private storedTopicsSub: Subscription;
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private coursesService: CoursesService
+    private coursesService: CoursesService,
   ) { }
 
   ngOnInit() {
@@ -30,31 +32,47 @@ export class TopicsPage implements OnInit, OnDestroy {
     this.coursesSub = this.coursesService.courses.subscribe(courses => {
       this.course = courses.find(course => course.id === courseId);
     });
-    this.fetchSub = this.getTopics(courseId).subscribe(
+    this.fetchSub = this.coursesService.fetchTopics(courseId).subscribe(
       topics => {
         if (!topics || topics.length === 0) {
           this.errorMessage = 'Coming soon';
+          this.isLoading = false;
+          return;
         }
         this.topics = topics;
         this.isLoading = false;
+        this.downloadSub = this.coursesService.downloadResources(courseId, topics).subscribe(
+          () => null,
+          downloadError => {
+            console.log('[ERROR] topics.page.ts#downloadResources', downloadError.message);
+          }
+        );
       }, error => {
-        console.log('[ERROR] topics.page.ts#ngOnInit', error.message);
-        this.errorMessage = error;
+        console.log('[ERROR] topics.page.ts#fetchTopics', error.message);
+        this.storedTopicsSub = this.coursesService.getTopicsFromStorage(courseId).subscribe(
+          topics => {
+            this.topics = topics;
+            this.isLoading = false;
+          },
+          storageError => {
+            console.log('[ERROR] topics.page.ts#getTopicsFromStorage', storageError.message);
+            this.errorMessage = storageError.message;
+            this.isLoading = false;
+          }
+        );
         this.isLoading = false;
       }
-    );
-  }
-
-  getTopics(courseId: number) {
-    return this.coursesService.fetchTopics(courseId).pipe(
-      catchError(() => {
-        return this.coursesService.getTopicsFromStorage(courseId);
-      })
     );
   }
 
   ngOnDestroy() {
     this.coursesSub.unsubscribe();
     this.fetchSub.unsubscribe();
+    if (this.downloadSub) {
+      this.downloadSub.unsubscribe();
+    }
+    if (this.storedTopicsSub) {
+      this.storedTopicsSub.unsubscribe();
+    }
   }
 }
