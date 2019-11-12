f3f83321003a3b297bea4b640086234b29c9a227
Warit Santaputra <waritsan@gmail.com>
2019-11-12 12:20:42 +0700

    Create new discussion

--- src/app/qa/qa-new/qa-new.page.ts
+++ src/app/qa/qa-new/qa-new.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
 import { QaService } from '../qa.service';
 
 @Component({
@@ -7,15 +10,27 @@ import { QaService } from '../qa.service';
   styleUrls: ['./qa-new.page.scss'],
 })
 export class QaNewPage implements OnInit {
+  @ViewChild('f', { static: false }) form: NgForm;
+  errorMessage = null;
 
   constructor(
+    private router: Router,
     private qaService: QaService
   ) { }
 
   ngOnInit() {}
 
   onCreateNewDiscussion() {
-    
+    if (!this.form.valid) {
+      return;
+    }
+    this.qaService.addDiscussion(this.form.value.subject, this.form.value.message).subscribe(success => {
+      if (success) {
+        this.router.navigate(['/tabs/qa']);
+      } else {
+        this.errorMessage = 'มีปัญหาในการตั้งคำถาม โปรดลองใหม่';
+      }
+    });
   }
 
 }
